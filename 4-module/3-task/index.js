f4d335257801dd15649ab9d796fd38a9978d06a6
LeoVT <tverikin.leonid@yandex.ru>
2020-11-01 19:48:08 +0300

    Решены задачи из занятия 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,28 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+    let rows = table.tBodies[0].rows;
+  
+    for (let i = 0; i < rows.length; i++) {
+      
+      if (+rows[i].cells[1].textContent < 18) { 
+        rows[i].style.textDecoration = 'line-through';
+      }
+      
+      if (rows[i].cells[2].textContent == 'm') { 
+        rows[i].classList.add('male');
+      } else rows[i].classList.add('female');
+      
+      if (rows[i].cells[3].hasAttribute('data-available')) {
+        
+        if (rows[i].cells[3].dataset.available == 'true') {
+          rows[i].classList.add('available');
+        } else rows[i].classList.add('unavailable');
+        
+      } else rows[i].hidden = true;
+      
+    }
+
 }
+    
