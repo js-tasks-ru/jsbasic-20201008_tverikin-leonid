a9893f897a9df4e72b218b83f734209852ddd454
LeoVT <tverikin.leonid@yandex.ru>
2020-11-10 13:37:09 +0300

    Решены задачи из Занятия 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,37 @@
  */
 export default class UserTable {
   constructor(rows) {
+    
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `<thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>`;
+  
+    let tbody = document.createElement('tbody');
+     
+    for (let i = 0; i < rows.length; i++) {   
+  
+      let tr = document.createElement('tr'); 
+   
+      for (let key in rows[i]) {
+        let td = document.createElement('td');
+        td.innerHTML = rows[i][key];
+        tr.append(td);
+      };
+    
+     let td = document.createElement('td');
+     td.innerHTML = `<button onclick ="this.closest('tr').remove()">X</button>`;
+     tr.append(td);
+     tbody.append(tr); 
+    }    
+   
+    this.elem.append(tbody); 
   }
-}
+  
+};
